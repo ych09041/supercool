09cdb29c4592f992dc407d63c6776dcad87daf0b
ych09041 <ych09041@gmail.com>
2016-04-02 19:51:07 -0700

    Test raw forward and reverse drive at slow speed.

--- boardTest/boardTest.ino
+++ boardTest/boardTest.ino
@@ -2,6 +2,8 @@ int INPUT1 = 5; //motor High-side Right-side
 int INPUT2 = 6; //motor High-side Left-side
 int INPUT3 = 9; //motor Low-side Left-side
 int INPUT4 = 10; //motor Low-side Right-side
+// 2 and 4 should be HIGH at the same time, 1 and 3 same time. NEVER OTHERWISE.
+// 3 and 4 HIGH together for braking. 1 and 2 LOW at this time.
 
 int LED_R = 11;
 int LED_G = 12;
@@ -10,6 +12,7 @@ int LED_B = 13;
 
 void setup() {
   // put your setup code here, to run once:
+  Serial.begin(115200);
   pinMode(INPUT1, OUTPUT);
   digitalWrite(INPUT1, LOW);
   pinMode(INPUT2, OUTPUT);
@@ -19,9 +22,6 @@ void setup() {
   pinMode(INPUT4, OUTPUT);
   digitalWrite(INPUT4, LOW);
   
-  
-  
-
   pinMode(LED_R,OUTPUT);
   pinMode(LED_G,OUTPUT);
   pinMode(LED_B,OUTPUT);
@@ -29,19 +29,59 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly:
-  digitalWrite(LED_G,HIGH);
-  delay(1);
-  digitalWrite(LED_R,HIGH);
-  delay(1);
-  digitalWrite(LED_B,HIGH);
-  delay(1);
   digitalWrite(LED_G,LOW);
-  delay(1);
   digitalWrite(LED_R,LOW);
-  delay(1);
   digitalWrite(LED_B,LOW);
-  delay(1);
-  
-  
+  motor_forward_raw(0.2);
+  delay(2000);
+  digitalWrite(LED_G,LOW);
+  digitalWrite(LED_R,LOW);
+  digitalWrite(LED_B,LOW);
+  motor_reverse_raw(0.2);
+  delay(2000);
   
 }
+
+void motor_forward_raw(float pwm) { // pwm var range 0.0-1.0
+  analogWrite(INPUT1, 0);
+  analogWrite(INPUT2, 0);
+  analogWrite(INPUT3, 0);
+  analogWrite(INPUT4, 0);
+  analogWrite(INPUT4, 255);
+  analogWrite(INPUT2, pwm_float2int(pwm));  
+}
+
+void motor_reverse_raw(float pwm) {
+  analogWrite(INPUT1, 0);
+  analogWrite(INPUT2, 0);
+  analogWrite(INPUT3, 0);
+  analogWrite(INPUT4, 0);
+  analogWrite(INPUT3, 255);
+  analogWrite(INPUT1, pwm_float2int(pwm)); 
+}
+
+void motor_brake_raw() {
+  analogWrite(INPUT1, 0);
+  analogWrite(INPUT2, 0);
+  analogWrite(INPUT3, 0);
+  analogWrite(INPUT4, 0);
+  analogWrite(INPUT3, 255);
+  analogWrite(INPUT4, 255); 
+}
+
+int pwm_float2int(float in) {
+  int out = (int)(in*255.0);
+  Serial.println(out);
+  return out;
+}
+
+
+
+
+
+
+
+
+
+
+
